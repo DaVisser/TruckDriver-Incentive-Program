b648fd5ce0a03345f86d4f264a546d267c5085c2
Marcus Macias <maciasa@clemson.edu>
2024-02-11 01:10:31 -0500

    Added the following Fields: Trucking License Number, Phone Number. Also allowed for data inputted in 'Sign Up' fields to be added to database with the password being hashed and stored securely.

--- login-signup/src/Components/LoginSignup/LoginSignup.jsx
+++ login-signup/src/Components/LoginSignup/LoginSignup.jsx
@@ -12,6 +12,60 @@ const LoginSignup = () => {
     const [password, setPassword] = useState('');
     const [phoneNumber, setPhoneNumber] = useState('');
     const [licenseNum, setLicense] = useState('');
+    const [attemptedSubmit, setAttemptedSubmit] = useState(false);
+
+
+
+    // Function to handle form submission
+    const handleSubmit = async (event) => {
+        event.preventDefault(); // Prevent default form submission behavior
+        setAttemptedSubmit(true);
+        const userData = { firstName, lastName, email, password, licenseNum, phoneNumber, action };
+
+        // Check if all required fields are filled. If not then dont proceed with fetch calls
+        if (!firstName || !lastName || !email || !password || (action === "Sign Up" && (!phoneNumber || !licenseNum))) {
+            return;
+        }
+
+        try {
+            //Currently fetches to the locally running server that connects to the database. Will change when we have EC2 running.
+            const response = await fetch('http://localhost:8081/signup', {
+                method: 'POST', // or 'GET' if you're retrieving data
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userData),
+            });
+
+            if (!response.ok) throw new Error('Network response was not ok');
+
+            const data = await response.json();
+            console.log(data); // Handle success
+        } catch (error) {
+            console.error('There was a problem with your fetch operation:', error);
+        }
+    };
+    //Function to handle switching from Login to Sign Up to clear fields
+    const switchToSignUp = () => {
+        setAction("Sign Up");
+        // Reset all fields
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setPassword('');
+        setPhoneNumber('');
+        setLicense('');
+        setAttemptedSubmit(false);
+    };
+    //Function to handle switching from Sign Up to Login to clear fields
+    const switchToLogin = () => {
+        setAction("Login");
+        // Reset only fields not relevant to login or all if preferred
+        setEmail('');
+        setPassword('');
+        setAttemptedSubmit(false); 
+    };
+
     return (
         <div className='container'>
             <div className="header">
@@ -21,44 +75,57 @@ const LoginSignup = () => {
             <div className="inputs">
 
                 {action === "Sign Up" ? 
-                <div className="input">
-                        <img src={user_icon} alt=""/>
-                        <input type="First Name" placeholder="First name" value={firstName} onChange={e => setFirstName(e.target.value)}/>
+                <div className={`${attemptedSubmit && !firstName ? 'input-error' : 'input'}`}>
+                    <img src={user_icon} alt=""/>
+                    <input type="text" placeholder="First Name" value={firstName} onChange={e => setFirstName(e.target.value)}/>
                 </div>:<div></div>
                 }
+            
                 {action === "Sign Up" ? 
-                <div className="input">
+                <div className={`${attemptedSubmit && !lastName ? 'input-error' : 'input'}`}>
                         <img src={user_icon} alt=""/>
                         <input type="Last Name" placeholder="Last Name" value={lastName} onChange={e => setLastName(e.target.value)}/>
                 </div>:<div></div>
                     
                 }
                 {action === "Sign Up" ? 
-                <div className="input">
+                <div className={`${attemptedSubmit && !licenseNum ? 'input-error' : 'input'}`}>
                         <img src={password_icon} alt=""/>
                         <input type="License Number" placeholder="Trucking License Number" value={licenseNum} onChange={e => setLicense(e.target.value)}/>
                 </div>:<div></div>
                 }
-
+                
                 {action === "Sign Up" ? 
-                <div className="input">
+                <div className={`${attemptedSubmit && !phoneNumber ? 'input-error' : 'input'}`}>
                         <img src={password_icon} alt=""/>
-                        <input type="Phone Number" placeholder="Phone Number" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)}/>
+                        <input type="tel" placeholder="Phone Number" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)}/>
                 </div>:<div></div>
                 }
-                <div className="input">
+                {action === "Sign Up" || "Login" ? 
+                <div className={`${attemptedSubmit && !email ? 'input-error' : 'input'}`}>
                     <img src={email_icon} alt=""/>
                     <input type="email" placeholder="Email Id" value={email} onChange={e => setEmail(e.target.value)}/>
-                </div>
-                <div className="input">
+                </div>:<div></div>
+                }
+                
+                
+                {action === "Sign Up" || "Login" ? 
+                <div className={`${attemptedSubmit && !password ? 'input-error' : 'input'}`}>
                     <img src={password_icon} alt=""/>
                     <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}/>
-                </div>
+                </div>:<div></div>
+                }
+                
             </div>
-            {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost Password? <span>Click Here</span></div>}
+            
             <div className="submit-container">
-                <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => setAction("Sign Up")}>Sign Up</div>
-                <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => setAction("Login")}>Login</div>
+                <button className = "submit" type="button" onClick={handleSubmit}>{action}</button>
+            </div>
+            
+            {action === "Login"?<div className="forgot-password">Lost Password? <span>Click Here</span></div>:<div></div>}
+            <div className="submit-container">
+                <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={switchToSignUp}>Sign Up</div>
+                <div className={action === "Login" ? "submit gray" : "submit"} onClick={switchToLogin}>Login</div>
             </div>
         </div>
     );
