667853180b3b5f1b1587f53b51e5983b3d265bee
Mark H Youssef <mhyouss@g.clemson.edu>
2024-02-19 11:28:16 -0500

    changed userpool

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -39,9 +39,9 @@ function Welcome() {
         <div>
             <h3>Welcome to HDM Solutions Trucking Rewards!</h3>
             <p>
-                <Link to="https://team6.auth.us-east-1.amazoncognito.com/login?client_id=4nfvf9mnln2tv6qbf6hca9p8te&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin">Register or Login</Link>
+                <Link to="https://team06.auth.us-east-1.amazoncognito.com/login?client_id=6ug5o2oo8pgg3t1o9e5h81sgee&response_type=code&scope=email+openid+phone&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin">Register or Login</Link>
                 <br></br>
-                <Link to="https://team6.auth.us-east-1.amazoncognito.com/logout?client_id=4nfvf9mnln2tv6qbf6hca9p8te&email+openid+profile&logout_uri=http%3A%2F%2Flocalhost%3A3000%2Flogout">Log back out</Link>
+                <Link to="https://team06.auth.us-east-1.amazoncognito.com/logout?client_id=6ug5o2oo8pgg3t1o9e5h81sgee&email+openid+phone&logout_uri=http%3A%2F%2Flocalhost%3A3000%2Flogout">Log back out</Link>
                 <br></br>
             </p>
         </div>
--- Frontend/src/Components/CognitoSignup/logged_in.jsx
+++ Frontend/src/Components/CognitoSignup/logged_in.jsx
@@ -6,7 +6,7 @@ function LoggedInComponent() {
         <div>
             <h1>Congratulations!</h1>
             <p>You are logged in now!</p>
-            <Link to="https://team6.auth.us-east-1.amazoncognito.com/logout?client_id=4nfvf9mnln2tv6qbf6hca9p8te&email+openid+profile&logout_uri=http%3A%2F%2Flocalhost%3A3000%2Flogout">Log back out</Link>
+            <Link to="https://team06.auth.us-east-1.amazoncognito.com/logout?client_id=6ug5o2oo8pgg3t1o9e5h81sgee&email+openid+phone&logout_uri=http%3A%2F%2Flocalhost%3A3000%2Flogout">Log back out</Link>
         </div>
     );
 }
