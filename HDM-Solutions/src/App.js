4a0805e258db6b50077c62eda17d5ec99c7209a2
Mark H Youssef <mhyouss@g.clemson.edu>
2024-02-25 17:39:13 -0500

    working login that takes to about page

--- HDM-Solutions/src/App.js
+++ HDM-Solutions/src/App.js
@@ -279,10 +279,22 @@ const components = {
     },
   };
   
-  export default function App() {
+export default function App() {
     return (
-      <Authenticator formFields={formFields} components={components}>
-        {({ signOut }) => <button onClick={signOut}>Sign out</button>}
-      </Authenticator>
+        <Authenticator formFields={formFields} components={components}>
+            {({ signOut }) => (
+                <Router>
+                    <div>
+                        <nav>
+                            <Link to="/">Home</Link> | <Link to="/about">About</Link>
+                        </nav>
+                        <Routes>
+                            <Route path="/" element={<button onClick={signOut}>Sign out</button>} />
+                            <Route path="/about" element={<AboutPage signOut={signOut} />} />
+                        </Routes>
+                    </div>
+                </Router>
+            )}
+        </Authenticator>
     );
-  }
+}
