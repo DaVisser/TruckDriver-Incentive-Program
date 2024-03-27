4711af3b6e5f3fb1ace668cdebc755716ca91db0
Daniel Visser <dvisser@clemson.edu>
2024-03-27 13:07:55 -0400

    Usermanagement jsx file added

---
+++ HDM-Solutions/src/Components/UserManagement/UserManagement.jsx
@@ -0,0 +1,53 @@
+// src/Components/UserManagement/UserManagement.jsx
+import React, { useState, useEffect } from 'react';
+import './UserManagement.css';
+
+const UserManagement = () => {
+    const [users, setUsers] = useState([]);
+
+    const fetchUsers = async () => {
+        try {
+            const response = await fetch('https://cqf7mwevac.execute-api.us-east-1.amazonaws.com/dev/users'); // Replace INVOKE_URL_HERE with your actual invoke URL
+            const data = await response.json();
+            setUsers(data);
+        } catch (error) {
+            console.error('Error fetching users:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    return (
+        <div>
+            <h1>User Management</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Delete User</th>
+                        <th>Username</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map(user => (
+                        <tr key={user.UserId}>
+                            <td><button>Delete</button></td>
+                            <td>{user.UserName}</td>
+                            <td>{user.FirstName}</td>
+                            <td>{user.LastName}</td>
+                            <td>{user.Email}</td>
+                            <td>{user.Role}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default UserManagement;
